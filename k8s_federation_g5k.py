8cd503797b9de9c9c5a7798179292a6500ba4792
UoFixED <42113547+UoFixED@users.noreply.github.com>
2022-04-22 22:49:13 +0200

    ww

--- k8s_federation_g5k.py
+++ k8s_federation_g5k.py
@@ -25,7 +25,7 @@ for i in range(0, len(clusters)):
     
     conf = Configuration.from_settings(job_name=name_job,
                                        walltime=duration,
-                                       image="/grid5000/virt-images/ubuntu2004-x64-min.qcow2")\
+                                       image="/grid5000/virt-images/ubuntu2004-x64-min-2022032913.qcow2")\
                         .add_machine(roles=[role_name],
                                      cluster=clusters[i],
                                      flavour_desc={"core": 2, "mem": 4096},
