ff8f7ad9a879d7896dd8a544c920bffcea852f1a
UoFixED <42113547+UoFixED@users.noreply.github.com>
2022-04-23 01:25:16 +0200

    Update k8s_federation_g5k.py

--- k8s_federation_g5k.py
+++ k8s_federation_g5k.py
@@ -45,7 +45,7 @@ for i in range(0, len(clusters)):
 
     # Make sure k8s is not already running
     #run_ansible(["reset_k8s.yml"], inventory_path=inventory_file)
-
+    time.sleep(10)
     # Deploy k8s and dependencies
     run_ansible(["deploy_system.yml"], inventory_path=inventory_file)
 
