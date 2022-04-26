c49d725fddc04d32bdad763042ff7828f7eaefa6
UoFixED <42113547+UoFixED@users.noreply.github.com>
2022-04-26 21:40:21 +0200

    www

--- 01_auto.sh
+++ 01_auto.sh
@@ -1,2 +1,3 @@
+chmod 777 02_system_ready.sh
 python3 management.py &
 python3 member.py &
--- 02_system_ready.sh
+++ 02_system_ready.sh
@@ -3,13 +3,13 @@ i=0
 manage=$(awk NR==1 node_list)
 git clone https://github.com/CKHuangGH/mck8s_vm.git
 git clone https://github.com/CKHuangGH/rntsm.git
-scp -r /home/chuang/k8s_g5k/mck8s_vm root@$manage:/root/
-scp -r /home/chuang/k8s_g5k/rntsm root@$manage:/root/
 scp node_list root@$manage:/root/mck8s_vm
 rm -rf /home/chuang/.ssh/known_hosts
 
 for j in $(cat node_list)
 do
+scp -r /home/chuang/k8s_g5k/mck8s_vm root@$j:/root/
+scp -r /home/chuang/k8s_g5k/rntsm root@$j:/root/
 scp /home/chuang/.ssh/id_rsa root@$j:/root/.ssh
 ssh -o StrictHostKeyChecking=no root@$j mv /root/.kube/config /root/.kube/cluster$i
 ssh -o StrictHostKeyChecking=no root@$j scp -o StrictHostKeyChecking=no /root/.kube/cluster$i root@$manage:/root/.kube
@@ -20,6 +20,9 @@ ssh -o StrictHostKeyChecking=no root@$j chmod 777 /root/mck8s_vm/04_mck8s-2.sh
 ssh -o StrictHostKeyChecking=no root@$j chmod 777 /root/mck8s_vm/05_joining_test.sh
 ssh -o StrictHostKeyChecking=no root@$j chmod 777 /root/mck8s_vm/06_deployment.sh
 ssh -o StrictHostKeyChecking=no root@$j chmod 777 /root/mck8s_vm/createvalue.sh
+ssh -o StrictHostKeyChecking=no root@$j chmod 777 /root/mck8s_vm/results/01_status.sh
+ssh -o StrictHostKeyChecking=no root@$j chmod 777 /root/mck8s_vm/results/02_run.sh
+ssh -o StrictHostKeyChecking=no root@$j chmod 777 /root/mck8s_vm/results/03_cptorennes.sh
 i=$((i+1))
 done
 
