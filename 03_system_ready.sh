012e7e1c538a4b13bab5a9ea18332fd65439a89a
CKHuangGH <chihkaihuang1@gmail.com>
2022-04-27 12:32:55 +0200

    Update 03_system_ready.sh

--- 03_system_ready.sh
+++ 03_system_ready.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 i=0
 manage=$(awk NR==1 node_list)
-#git clone https://github.com/CKHuangGH/mck8s_vm.git
-#git clone https://github.com/CKHuangGH/rntsm.git
-scp node_list root@$manage:/root/mck8s_vm/node_list
+git clone https://github.com/CKHuangGH/mck8s_vm.git
+git clone https://github.com/CKHuangGH/rntsm.git
 rm -rf /home/chuang/.ssh/known_hosts
 
 for j in $(cat node_list)
 do
-ssh -o StrictHostKeyChecking=no root@$j git clone https://github.com/CKHuangGH/mck8s_vm.git
-ssh -o StrictHostKeyChecking=no root@$j git clone https://github.com/CKHuangGH/rntsm.git
+scp -r /home/chuang/k8s_g5k/mck8s_vm root@$j:/root/
+scp -r /home/chuang/k8s_g5k/rntsm root@$j:/root/
 scp /home/chuang/.ssh/id_rsa root@$j:/root/.ssh
 ssh -o StrictHostKeyChecking=no root@$j mv /root/.kube/config /root/.kube/cluster$i
 ssh -o StrictHostKeyChecking=no root@$j scp -o StrictHostKeyChecking=no /root/.kube/cluster$i root@$manage:/root/.kube
@@ -25,5 +24,5 @@ ssh -o StrictHostKeyChecking=no root@$j chmod 777 /root/mck8s_vm/results/02_run.
 ssh -o StrictHostKeyChecking=no root@$j chmod 777 /root/mck8s_vm/results/03_cptorennes.sh
 i=$((i+1))
 done
-
+scp node_list root@$manage:/root/mck8s_vm/node_list
 echo "management node is $manage"
