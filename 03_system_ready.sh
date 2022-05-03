991869a99a1a4214f788ef3f82546cce8fc77b93
UoFixED <42113547+UoFixED@users.noreply.github.com>
2022-05-03 18:47:16 +0200

    Update 03_system_ready.sh

--- 03_system_ready.sh
+++ 03_system_ready.sh
@@ -10,8 +10,8 @@ do
 scp -r /home/chuang/k8s_g5k/mck8s_vm root@$j:/root/
 scp -r /home/chuang/k8s_g5k/rntsm root@$j:/root/
 scp /home/chuang/.ssh/id_rsa root@$j:/root/.ssh
-ssh -o StrictHostKeyChecking=no root@$j mv /root/.kube/config /root/.kube/cluster$i
-ssh -o StrictHostKeyChecking=no root@$j scp -o StrictHostKeyChecking=no /root/.kube/cluster$i root@$manage:/root/.kube
+#ssh -o StrictHostKeyChecking=no root@$j mv /root/.kube/config /root/.kube/cluster$i
+ssh -o StrictHostKeyChecking=no root@$j scp -o StrictHostKeyChecking=no /root/.kube/config root@$manage:/root/.kube/cluster$i
 ssh -o StrictHostKeyChecking=no root@$j chmod 777 /root/mck8s_vm/01_combineAll.sh
 ssh -o StrictHostKeyChecking=no root@$j chmod 777 /root/mck8s_vm/02_rntsmvalue.sh
 ssh -o StrictHostKeyChecking=no root@$j chmod 777 /root/mck8s_vm/03_createmember.sh
