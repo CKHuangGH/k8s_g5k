6dc4fc12f1b5073163d52c4e3132e5839d2d0651
UoFixED <42113547+UoFixED@users.noreply.github.com>
2022-09-25 18:58:25 +0200

    Update 01_deploy.sh

--- mcdeploy/01_deploy.sh
+++ mcdeploy/01_deploy.sh
@@ -1,9 +1,14 @@
 python3 management.py &
-sleep 30
+sleep 40
 python3 m1.py &
-sleep 30
+sleep 40
 python3 m2.py &
-sleep 30
+sleep 40
 python3 m3.py &
 chmod 777 02_system_ready.sh
 chmod 777 03_del.sh
+
+for j in $(cat node_list)
+do
+ssh -o StrictHostKeyChecking=no root@$j kubectl get node
+done
