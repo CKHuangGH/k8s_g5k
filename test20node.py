effcc1b3d20fe98c9e5b486ed5fc3f70295f1a6a
CKHuangGH <chihkaihuang1@gmail.com>
2022-09-30 12:40:07 +0200

    Update test20node.py

--- test20node.py
+++ test20node.py
@@ -4,11 +4,18 @@ from enoslib.infra.enos_vmong5k.provider import VMonG5k
 from enoslib.infra.enos_vmong5k.configuration import Configuration
 
 import logging
+from pathlib import Path
+
+import enoslib as en
+
+import logging
 import time
 
 name = "member"
 
-clusters = ["paravance"]
+clusters = "paravance"
+
+site = "rennes"
 
 #logging.basicConfig(level=logging.DEBUG)
 
@@ -16,65 +23,65 @@ master_nodes = []
 
 duration = "05:00:00"
 
-prod_network = en.G5kNetworkConf(type="prod", roles=["my_network"], site=SITE)
-for i in range(0, len(clusters)):
+prod_network = en.G5kNetworkConf(type="prod", roles=["my_network"], site=site)
 
-    name_job = name + clusters[i] + str(i)
+name_job = name + clusters
 
-    role_name = "cluster" + str(clusters[i])
-    
-    conf = (
-        en.G5kConf.from_settings(job_type="allow_classic_ssh", job_name=job_name)
-        .add_network_conf(prod_network)
-        .add_network(
-            id="not_linked_to_any_machine", type="slash_22", roles=["my_subnet"], site=SITE
-        )
-        .add_machine(
-        roles=["role1"], cluster=i, nodes=1, primary_network=prod_network
-        )
-        .finalize()
+role_name = "cluster" + str(clusters)
+
+conf = (
+    en.G5kConf.from_settings(job_type="allow_classic_ssh", job_name=name_job)
+    .add_network_conf(prod_network)
+    .add_network(
+        id="not_linked_to_any_machine", type="slash_22", roles=["my_subnet"], site=site
     )
-    provider = en.G5k(conf)
-    roles, networks = provider.init()
-    roles = en.sync_info(roles, networks)
-    
-    subnet = networks["my_subnet"][0]
-    cp = 1
-    w=20
-    virt_conf = (
-        en.VMonG5kConf.from_settings(image="/grid5000/virt-images/ubuntu2004-x64-min-2022032913.qcow2")
-        .add_machine(
-            roles=["cp"],
-            number=cp,
-            undercloud=roles["role1"],
-            flavour_desc={"core": 2, "mem": 8192},
-            macs=list(subnet.free_macs)[0:1]
-        .add_machine(
-            roles=["member"],
-            number=w,
-            undercloud=roles["role1"],
-            flavour_desc={"core": 1, "mem": 4096},
-        macs=list(subnet.free_macs)[1:w+1]
-        ).finalize()
+    .add_machine(
+    roles=["role1"], cluster=clusters, nodes=1, primary_network=prod_network
     )
-    
-    vmroles = en.start_virtualmachines(virt_conf)
+    .finalize()
+)
+provider = en.G5k(conf)
+roles, networks = provider.init()
+roles = en.sync_info(roles, networks)
 
-    print(vmroles)
+subnet = networks["my_subnet"][0]
+cp = 1
+w=20
+virt_conf = (
+    en.VMonG5kConf.from_settings(image="/grid5000/virt-images/ubuntu2004-x64-min-2022032913.qcow2")
+    .add_machine(
+        roles=["cp"],
+        number=cp,
+        undercloud=roles["role1"],
+        flavour_desc={"core": 2, "mem": 8192},
+        macs=list(subnet.free_macs)[0:1]
+    .add_machine(
+        roles=["member"],
+        number=w,
+        undercloud=roles["role1"],
+        flavour_desc={"core": 1, "mem": 4096},
+    macs=list(subnet.free_macs)[1:w+1]
+    ).finalize()
+)
+)
 
-    print(networks)
-    
-    inventory_file = "kubefed_inventory_cluster" + str(i) + ".ini" 
+vmroles = en.start_virtualmachines(virt_conf)
 
-    inventory = generate_inventory(roles, networks, inventory_file)
+print(vmroles)
 
-    master_nodes.append(roles[role_name][0].address)
+print(networks)
 
-    # Make sure k8s is not already running
-    #run_ansible(["reset_k8s.yml"], inventory_path=inventory_file)
-    time.sleep(30)
-    # Deploy k8s and dependencies
-    run_ansible(["deploy_system.yml"], inventory_path=inventory_file)
+inventory_file = "kubefed_inventory_cluster" + str(i) + ".ini" 
+
+inventory = generate_inventory(roles, networks, inventory_file)
+
+master_nodes.append(roles[role_name][0].address)
+
+# Make sure k8s is not already running
+#run_ansible(["reset_k8s.yml"], inventory_path=inventory_file)
+time.sleep(30)
+# Deploy k8s and dependencies
+run_ansible(["deploy_system.yml"], inventory_path=inventory_file)
 
 f = open("node_list", 'a')
 f.write(str(master_nodes[0]))
