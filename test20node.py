80eff5617d84181f5840c3b8cac592e27f4077b9
CKHuangGH <chihkaihuang1@gmail.com>
2022-09-30 12:45:24 +0200

    Update test20node.py

--- test20node.py
+++ test20node.py
@@ -54,7 +54,7 @@ virt_conf = (
         number=cp,
         undercloud=roles["role1"],
         flavour_desc={"core": 2, "mem": 8192},
-        macs=list(subnet.free_macs)[0:1]
+        macs=list(islice(subnet[0].free_macs, cp))
     .add_machine(
         roles=["member"],
         number=w,
