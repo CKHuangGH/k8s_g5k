b60da986e67a325aa137cfee8822e47917e8fd74
CKHuangGH <chihkaihuang1@gmail.com>
2022-10-04 11:52:43 +0200

    Update 04_cptorennes.sh

--- buildqcow2/04_cptorennes.sh
+++ buildqcow2/04_cptorennes.sh
@@ -2,5 +2,9 @@ virsh list
 virsh shutdown virtual-158-0-2
 sleep 5
 cd /var/lib/libvirt/images/enos_vmong5
-
+cp enos_vmong5k-base-image.qcow2 new-master
+qemu-img rebase -p -b new-master virtual-158-0-2
+qemu-img commit virtual-158-0-2
+qemu-img convert -p -c -O qcow2 new-master images.qcow2
+qemu-img info new-master
 scp -o StrictHostKeyChecking=no -r images.qcow2 chuang@172.16.111.106:/home/chuang/images
