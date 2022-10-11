20f0b9a4c5c3186ec18f3eafb5f3634a97421c90
CKHuangGH <chihkaihuang1@gmail.com>
2022-10-11 12:22:30 +0200

    for1

--- large/01_deploy.sh
+++ large/01_deploy.sh
@@ -1,10 +1,6 @@
 python3 ./clusters/management.py &
 sleep 1
 python3 ./clusters/m1.py &
-sleep 4
-python3 ./clusters/m2.py &
-sleep 4
-python3 ./clusters/m3.py &
 
 chmod 777 02_check.sh
 chmod 777 03_system_ready.sh
