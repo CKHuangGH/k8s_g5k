481b1b5ca095468cac156c7927eb29018ce1c236
UoFixED <42113547+UoFixED@users.noreply.github.com>
2022-10-12 21:21:06 +0200

    20

--- large-parasilo/01_deploy.sh
+++ large-parasilo/01_deploy.sh
@@ -19,6 +19,26 @@ sleep 5
 python3 ./clusters/m9.py &
 sleep 5
 python3 ./clusters/m10.py &
+sleep 5
+python3 ./clusters/m11.py &
+sleep 5
+python3 ./clusters/m12.py &
+sleep 5
+python3 ./clusters/m13.py &
+sleep 5
+python3 ./clusters/m14.py &
+sleep 5
+python3 ./clusters/m15.py &
+sleep 5
+python3 ./clusters/m16.py &
+sleep 5
+python3 ./clusters/m17.py &
+sleep 5
+python3 ./clusters/m18.py &
+sleep 5
+python3 ./clusters/m19.py &
+sleep 5
+python3 ./clusters/m20.py &
 
 chmod 777 02_check.sh
 chmod 777 03_system_ready.sh
