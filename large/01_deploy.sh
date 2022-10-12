2190ba2d00845cc0ee0db7b5214b88a56f1e2325
CKHuangGH <chihkaihuang1@gmail.com>
2022-10-12 15:27:25 +0200

    test

--- large/01_deploy.sh
+++ large/01_deploy.sh
@@ -1,23 +1,23 @@
 python3 ./clusters/management.py &
 sleep 10
 python3 ./clusters/m1.py &
-sleep 4
+sleep 5
 python3 ./clusters/m2.py &
-sleep 4
+sleep 5
 python3 ./clusters/m3.py &
-sleep 4
+sleep 5
 python3 ./clusters/m4.py &
-sleep 4
+sleep 5
 python3 ./clusters/m5.py &
-sleep 4
+sleep 5
 python3 ./clusters/m6.py &
-sleep 4
+sleep 5
 python3 ./clusters/m7.py &
-sleep 4
+sleep 5
 python3 ./clusters/m8.py &
-sleep 4
+sleep 5
 python3 ./clusters/m9.py &
-sleep 4
+sleep 5
 python3 ./clusters/m10.py &
 
 chmod 777 02_check.sh
