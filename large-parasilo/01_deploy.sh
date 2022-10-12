4af4fed43cb0ebd7662bce6f8e9fe3505eb07d4d
CKHuangGH <chihkaihuang1@gmail.com>
2022-10-12 10:41:38 +0200

    10 clusters

--- large-parasilo/01_deploy.sh
+++ large-parasilo/01_deploy.sh
@@ -1,6 +1,24 @@
 python3 ./clusters/management.py &
-sleep 1
+sleep 4
 python3 ./clusters/m1.py &
+sleep 4
+python3 ./clusters/m2.py &
+sleep 4
+python3 ./clusters/m3.py &
+sleep 4
+python3 ./clusters/m4.py &
+sleep 4
+python3 ./clusters/m5.py &
+sleep 4
+python3 ./clusters/m6.py &
+sleep 4
+python3 ./clusters/m7.py &
+sleep 4
+python3 ./clusters/m8.py &
+sleep 4
+python3 ./clusters/m9.py &
+sleep 4
+python3 ./clusters/m10.py &
 
 chmod 777 02_check.sh
 chmod 777 03_system_ready.sh
