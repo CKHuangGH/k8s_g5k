b3d442a1c22ff58e31d63effcfac8eb3f57d3ca6
UoFixED <42113547+UoFixED@users.noreply.github.com>
2023-02-18 13:51:43 +0100

    auto03

--- mcdeploy-1/01_deploy.sh
+++ mcdeploy-1/01_deploy.sh
@@ -8,8 +8,11 @@ python3 m3.py &
 sleep 5
 python3 m4.py &
 sleep 5
-python3 m5.py &
+python3 m5.py
+sleep 5
 
 chmod 777 02_check.sh
 chmod 777 03_system_ready.sh
 chmod 777 04_del.sh
+
+./ 03_system_ready.sh
