b2c0e6272023a313bebeb73410f624e9f31a4466
UoFixED <42113547+UoFixED@users.noreply.github.com>
2023-02-26 01:41:34 +0100

    10 to 8

--- mcdeploy-2/m5.py
+++ mcdeploy-2/m5.py
@@ -46,7 +46,7 @@ roles = en.sync_info(roles, networks)
 
 subnet = networks["my_subnet"]
 cp = 1
-w=10
+w=8
 virt_conf = (
     en.VMonG5kConf.from_settings(image="/home/chuang/images/images.qcow2")
     .add_machine(
