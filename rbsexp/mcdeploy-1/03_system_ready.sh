8e8c480cc1dd3ef4396c11ad5e9fa6aa550b6a04
CKHuangGH <chihkaihuang1@gmail.com>
2023-05-04 16:17:58 +0200

    use new repo

--- rbsexp/mcdeploy-1/03_system_ready.sh
+++ rbsexp/mcdeploy-1/03_system_ready.sh
@@ -1,41 +1,25 @@
 #!/bin/bash
 i=0
 manage=$(awk NR==1 node_list)
-git clone https://github.com/CKHuangGH/mck8s_vm.git
-#git clone https://github.com/CKHuangGH/acala.git
+git clone https://github.com/CKHuangGH/exprbs.git
+git clone https://github.com/CKHuangGH/acala.git
 git clone https://github.com/CKHuangGH/k8s_google_injection.git
-git clone https://github.com/CKHuangGH/DASI.git
+#git clone https://github.com/CKHuangGH/DASI.git
 #git clone https://github.com/CKHuangGH/acala_error.git
 rm -rf /home/chuang/.ssh/known_hosts
 
 for j in $(cat node_list)
 do
 #scp ./install.sh root@$j:/root/
-scp -r ./mck8s_vm root@$j:/root/
-#scp -r ./acala root@$j:/root/
+scp -r ./exprbs root@$j:/root/
+scp -r ./acala root@$j:/root/
 scp -r ./k8s_google_injection root@$j:/root/
-scp -r ./DASI root@$j:/root/
+#scp -r ./DASI root@$j:/root/
 #scp -r ./acala_error root@$j:/root/
 scp /home/chuang/.ssh/id_rsa root@$j:/root/.ssh
-#ssh -o StrictHostKeyChecking=no root@$j chmod 777 ./install.sh
-#ssh -o StrictHostKeyChecking=no root@$j sh ./install.sh &
-# ssh -o StrictHostKeyChecking=no root@$j sudo apt-get install vim -y
-#ssh -o StrictHostKeyChecking=no root@$j sudo apt install python3-pip -y
-# ssh -o StrictHostKeyChecking=no root@$j sudo pip3 install pandas
-#ssh -o StrictHostKeyChecking=no root@$j sudo pip3 install kubernetes
-# ssh -o StrictHostKeyChecking=no root@$j sudo pip3 install aiohttp
-# #ssh -o StrictHostKeyChecking=no root@$j mv /root/.kube/config /root/.kube/cluster$i
 ssh -o StrictHostKeyChecking=no root@$j scp -o StrictHostKeyChecking=no /root/.kube/config root@$manage:/root/.kube/cluster$i
-#ssh -o StrictHostKeyChecking=no root@$j chmod 777 -R /root/mck8s_vm/large-scale/
-ssh -o StrictHostKeyChecking=no root@$j chmod 777 -R /root/mck8s_vm/sasi/
-# ssh -o StrictHostKeyChecking=no root@$j chmod 777 /root/acala/member/build.sh
-# ssh -o StrictHostKeyChecking=no root@$j chmod 777 /root/mck8s_vm/large-scale/00_acala.sh
-# ssh -o StrictHostKeyChecking=no root@$j chmod 777 /root/mck8s_vm/large-scale/00_prom.sh
-# ssh -o StrictHostKeyChecking=no root@$j chmod 777 /root/mck8s_vm/large-scale/01_joining.sh
-# ssh -o StrictHostKeyChecking=no root@$j chmod 777 /root/mck8s_vm/large-scale/acalavalue.sh
-# ssh -o StrictHostKeyChecking=no root@$j chmod 777 /root/mck8s_vm/large-scale/combineAll.sh
-# ssh -o StrictHostKeyChecking=no root@$j chmod 777 /root/mck8s_vm/large-scale/mck8s-management.sh
-# ssh -o StrictHostKeyChecking=no root@$j chmod 777 /root/mck8s_vm/large-scale/promvalue.sh
+#ssh -o StrictHostKeyChecking=no root@$j chmod 777 -R /root/exprbs/large-scale/
+ssh -o StrictHostKeyChecking=no root@$j chmod 777 -R /root/exprbs/0-kubefed/
 i=$((i+1))
 done
 
@@ -49,7 +33,7 @@ done < node_list
 
 
 scp /home/chuang/.ssh/id_rsa root@10.$ip1.$ip2.3:/root/.ssh
-scp -r ./mck8s_vm root@10.$ip1.$ip2.3:/root/
-ssh -o StrictHostKeyChecking=no root@10.$ip1.$ip2.3 chmod 777 -R /root/mck8s_vm/sasi/
-scp node_list root@$manage:/root/mck8s_vm/sasi/node_list
+scp -r ./exprbs root@10.$ip1.$ip2.3:/root/
+ssh -o StrictHostKeyChecking=no root@10.$ip1.$ip2.3 chmod 777 -R /root/exprbs/sasi/
+scp node_list root@$manage:/root/exprbs/sasi/node_list
 echo "management node is $manage"
