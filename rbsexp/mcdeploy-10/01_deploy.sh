143a9ce927987565825939c3afe73c7f7a01c28c
UoFixED <42113547+UoFixED@users.noreply.github.com>
2023-05-30 20:26:50 +0200

    test

---
+++ rbsexp/mcdeploy-10/01_deploy.sh
@@ -0,0 +1,19 @@
+python3 management.py
+#sleep 35
+python3 m1.py &
+sleep 5
+python3 m2.py &
+sleep 5
+python3 m3.py &
+sleep 5
+python3 m4.py &
+sleep 5
+python3 m5.py
+
+sleep 30
+
+chmod 777 02_check.sh
+chmod 777 03_system_ready.sh
+chmod 777 04_del.sh
+
+. ./03_system_ready.sh
