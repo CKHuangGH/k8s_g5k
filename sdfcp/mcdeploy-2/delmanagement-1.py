890421eb965065b9c08b0b4ae5e52918f16b3af4
UoFixED <42113547+UoFixED@users.noreply.github.com>
2023-05-30 21:04:46 +0200

    test

--- sdfcp/mcdeploy-2/delmanagement-1.py
+++ sdfcp/mcdeploy-2/delmanagement-1.py
@@ -6,9 +6,7 @@ from enoslib.infra.enos_vmong5k.configuration import Configuration
 import logging
 import time
 
-
-
-name = "mcdeploymaster-sdfcp-21"
+name = "mcdeploymaster-sdfcp-23"
 
 clusters = ["paravance"]
 
@@ -16,7 +14,7 @@ clusters = ["paravance"]
 
 master_nodes = []
 
-duration = "06:00:00"
+duration = "04:00:00"
 
 
 for i in range(0, len(clusters)):
@@ -27,31 +25,11 @@ for i in range(0, len(clusters)):
     
     conf = Configuration.from_settings(job_name=name_job,
                                        walltime=duration,
-                                       image="/home/chuang/images/images.qcow2")\
+                                       image="/grid5000/virt-images/ubuntu2004-x64-min-2022032913.qcow2")\
                         .add_machine(roles=[role_name],
                                      cluster=clusters[i],
                                      flavour_desc={"core": 4, "mem": 16384},
                                      number=2)\
                         .finalize()
     provider = VMonG5k(conf)
-
-    roles, networks = provider.init()
-
-    inventory_file = "kubefed_inventory_cluster" + str(name_job) + ".ini" 
-
-    inventory = generate_inventory(roles, networks, inventory_file)
-
-    master_nodes.append(roles[role_name][0].address)
-
-    # Make sure k8s is not already running
-    #run_ansible(["reset_k8s.yml"], inventory_path=inventory_file)
-    time.sleep(45)
-    # Deploy k8s and dependencies
-    run_ansible(["afterbuild.yml"], inventory_path=inventory_file)
-    
-f = open("node_list", 'a')
-f.write(str(master_nodes[0]))
-f.write("\n")
-f.close
-print("Master nodes ........")
-print(master_nodes)
+    provider.destroy()
--- sdfcp/mcdeploy-2/m1.py
+++ sdfcp/mcdeploy-2/m1.py
@@ -11,7 +11,7 @@ import enoslib as en
 import logging
 import time
 
-name = "m1-sdfcp"
+name = "m1-sdfcp-2"
 
 clusters = "paravance"
 
