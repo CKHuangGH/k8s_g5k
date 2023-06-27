03683b6bd5d933bc9f79299734cf856ec3595106
UoFixED <42113547+UoFixED@users.noreply.github.com>
2023-06-27 23:37:35 +0200

    Update testst.py

--- back/buildqcow2/testst.py
+++ back/buildqcow2/testst.py
@@ -25,6 +25,7 @@ for i in range(0, len(clusters)):
     
     conf = Configuration.from_settings(job_name=name_job,
                                        walltime=duration,
+                                       job_type=["deploy"],
                                        env_name="ubuntu2204-min")\
                         .add_machine(roles=[role_name],
                                      cluster=clusters[i],
