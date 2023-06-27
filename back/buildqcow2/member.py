8aaa8590a6f533994d9c004dcf79477a09ade15d
UoFixED <42113547+UoFixED@users.noreply.github.com>
2023-06-27 19:37:11 +0200

    Update member.py

--- back/buildqcow2/member.py
+++ back/buildqcow2/member.py
@@ -25,10 +25,10 @@ for i in range(0, len(clusters)):
     
     conf = Configuration.from_settings(job_name=name_job,
                                        walltime=duration,
-                                       image="/grid5000/virt-images/ubuntu2204-x64-min.qcow2")\
+                                       image="/grid5000/virt-images/ubuntu2204-x64-min-2023042409.qcow2")\
                         .add_machine(roles=[role_name],
                                      cluster=clusters[i],
-                                     flavour_desc={"core": 2, "mem": 4096},
+                                     flavour_desc={"core": 4, "mem": 4096},
                                      number=1)\
                         .finalize()
     provider = VMonG5k(conf)
