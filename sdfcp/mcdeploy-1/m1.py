6f4b0329f673421dfcd92aee3cf0c96d10c864a7
UoFixED <42113547+UoFixED@users.noreply.github.com>
2023-08-21 00:45:18 +0200

    Update m1.py

--- sdfcp/mcdeploy-1/m1.py
+++ sdfcp/mcdeploy-1/m1.py
@@ -60,7 +60,7 @@ virt_conf = (
         roles=["member"],
         number=w,
         undercloud=roles["role1"],
-        flavour_desc={"core": 1, "mem": 4096},
+        flavour_desc={"core": 2, "mem": 4096},
         macs=list(subnet[0].free_macs)[1:w+1],
     ).finalize()
 )
