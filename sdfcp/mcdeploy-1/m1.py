bd37b87bc2158d8f890502e9d01b0953297088fc
CKHuangGH <chihkaihuang1@gmail.com>
2023-08-21 17:01:43 +0200

    Update m1.py

--- sdfcp/mcdeploy-1/m1.py
+++ sdfcp/mcdeploy-1/m1.py
@@ -36,7 +36,7 @@ conf = (
         id="not_linked_to_any_machine", type="slash_22", roles=["my_subnet"], site=site
     )
     .add_machine(
-    roles=["role1"], cluster=clusters, nodes=1, primary_network=prod_network
+    roles=["role1"], cluster=clusters, nodes=3, primary_network=prod_network
     )
     .finalize()
 )
