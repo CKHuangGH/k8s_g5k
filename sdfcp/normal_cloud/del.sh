05e7e41eca54360e782cdb85b95538baaf55ea8f
CKHuangGH <chihkaihuang1@gmail.com>
2023-08-22 13:04:02 +0200

    test

--- sdfcp/normal_cloud/del.sh
+++
@@ -1,8 +0,0 @@
-folders=("mcdeploy-1" "mcdeploy-2" "mcdeploy-3" "mcdeploy-4" "mcdeploy-5")
-
-for folder in "${folders[@]}"; do
-    cd "$folder" || exit 1 
-    chmod +x 04_del.sh     
-    ./04_del.sh            
-    cd ..                  
-done
