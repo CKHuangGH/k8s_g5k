28d10b942b44e0a79fbf897938fe3a7d84d461ae
UoFixED <42113547+UoFixED@users.noreply.github.com>
2023-08-24 02:14:33 +0200

    test

--- sdfcp/mcdeploy-3/management.py
+++ sdfcp/mcdeploy-3/management.py
@@ -16,7 +16,7 @@ clusters = ["paravance"]
 
 master_nodes = []
 
-duration = "08:00:00"
+duration = "15:00:00"
 
 
 for i in range(0, len(clusters)):
--- sdfcp/mcdeploy-4/m1.py
+++ sdfcp/mcdeploy-4/m1.py
@@ -21,7 +21,7 @@ site = "rennes"
 
 master_nodes = []
 
-duration = "08:00:00"
+duration = "15:00:00"
 
 prod_network = en.G5kNetworkConf(type="prod", roles=["my_network"], site=site)
 
