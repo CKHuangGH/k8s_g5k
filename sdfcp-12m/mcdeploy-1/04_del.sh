551842991f283b99ba37274565a96a4eba708ea2
UoFixED <42113547+UoFixED@users.noreply.github.com>
2023-10-05 01:14:41 +0200

    Update 04_del.sh

--- sdfcp-12m/mcdeploy-1/04_del.sh
+++ sdfcp-12m/mcdeploy-1/04_del.sh
@@ -1,4 +1,4 @@
-python3 delmanagement.py
-# python3 delmanagement-1.py 
-# python3 delmanagement-2.py
-python3 delm1.py
+python3 ./delcluster/delmanagement.py
+python3 ./delcluster/m1.py
+python3 ./delcluster/m2.py
+python3 ./delcluster/m3.py
