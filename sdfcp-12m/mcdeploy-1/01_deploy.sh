7981a49d82cad516b7cdf91c7194cb1238689e2f
CKHuangGH <chihkaihuang1@gmail.com>
2023-10-06 14:17:13 +0200

    Update 01_deploy.sh

--- sdfcp-12m/mcdeploy-1/01_deploy.sh
+++ sdfcp-12m/mcdeploy-1/01_deploy.sh
@@ -2,10 +2,10 @@ python3 ./cluster/management.py &
 sleep 30
 python3 ./cluster/m1.py &
 python3 ./cluster/m2.py &
-python3 ./cluster/m3.py
+python3 ./cluster/m3.py &
 python3 ./cluster/m4.py
-echo "wait for 30 secs"
-sleep 30
+echo "wait for 60 secs"
+sleep 60
 
 chmod 777 02_check.sh
 chmod 777 03_system_ready.sh
