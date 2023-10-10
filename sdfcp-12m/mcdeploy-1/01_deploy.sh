ceea2690795b8638258fe25912501333ea7b42da
UoFixED <42113547+UoFixED@users.noreply.github.com>
2023-10-10 17:33:27 +0200

    tse

--- sdfcp-12m/mcdeploy-1/01_deploy.sh
+++ sdfcp-12m/mcdeploy-1/01_deploy.sh
@@ -2,9 +2,17 @@ python3 ./cluster/management.py &
 sleep 30
 python3 ./cluster/m1.py &
 python3 ./cluster/m2.py &
-python3 ./cluster/m3.py
+python3 ./cluster/m3.py &
+python3 ./cluster/m4.py &
+python3 ./cluster/m5.py &
+python3 ./cluster/m6.py &
+python3 ./cluster/m7.py &
+python3 ./cluster/m8.py &
+python3 ./cluster/m9.py &
+python3 ./cluster/m10.py
+
 echo "wait for 60 secs"
-sleep 120
+sleep 300
 
 chmod 777 02_check.sh
 chmod 777 03_system_ready.sh
