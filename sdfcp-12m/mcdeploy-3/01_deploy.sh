85f4ab44ba5e656e5b2771d14ed936ea90074e24
CKHuangGH <95505429+CKHuangGH@users.noreply.github.com>
2023-10-14 12:51:45 +0200

    test

--- sdfcp-12m/mcdeploy-3/01_deploy.sh
+++ sdfcp-12m/mcdeploy-3/01_deploy.sh
@@ -9,7 +9,17 @@ python3 ./cluster/m6.py &
 python3 ./cluster/m7.py &
 python3 ./cluster/m8.py &
 python3 ./cluster/m9.py &
-python3 ./cluster/m10.py
+python3 ./cluster/m10.py &
+python3 ./cluster/m11.py &
+python3 ./cluster/m12.py &
+python3 ./cluster/m13.py &
+python3 ./cluster/m14.py &
+python3 ./cluster/m15.py &
+python3 ./cluster/m16.py &
+python3 ./cluster/m17.py &
+python3 ./cluster/m18.py &
+python3 ./cluster/m19.py &
+python3 ./cluster/m20.py
 
 echo "wait for 60 secs"
 sleep 300
