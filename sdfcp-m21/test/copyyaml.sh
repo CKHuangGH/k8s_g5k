9f48b046292d346f712ae29745bb16a464346505
CKHuangGH <chihkaihuang1@gmail.com>
2023-11-06 14:42:28 +0100

    Update copyyaml.sh

--- sdfcp-m21/test/copyyaml.sh
+++ sdfcp-m21/test/copyyaml.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
 manage=$(awk NR==1 node_list)
+scp -o StrictHostKeyChecking=no /home/chuang/.ssh/id_rsa root@$manage:/root/.ssh
 scp -o StrictHostKeyChecking=no conf.yaml root@$manage:/root/conf.yaml
