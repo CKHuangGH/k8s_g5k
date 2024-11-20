1385b50f604c6a8e39b82bcfde84e6ddda69fd43
CKHuangGH <chihkaihuang1@gmail.com>
2024-11-20 13:46:21 +0100

    dw

---
+++ back/2-clusters-parasilo/01_deploy.sh
@@ -0,0 +1,65 @@
+python3 ./clusters/management.py &
+sleep 10
+python3 ./clusters/m1.py &
+sleep 5
+# python3 ./clusters/m2.py &
+# sleep 5
+# python3 ./clusters/m3.py &
+# sleep 5
+# python3 ./clusters/m4.py &
+# sleep 5
+# python3 ./clusters/m5.py &
+# sleep 5
+# python3 ./clusters/m6.py &
+# sleep 5
+# python3 ./clusters/m7.py &
+# sleep 5
+# python3 ./clusters/m8.py &
+# sleep 5
+# python3 ./clusters/m9.py &
+# sleep 5
+# python3 ./clusters/m10.py &
+# sleep 5
+# python3 ./clusters/m11.py &
+# sleep 5
+# python3 ./clusters/m12.py &
+# sleep 5
+# python3 ./clusters/m13.py &
+# sleep 5
+# python3 ./clusters/m14.py &
+# sleep 5
+# python3 ./clusters/m15.py &
+# sleep 5
+# python3 ./clusters/m16.py &
+# sleep 5
+# python3 ./clusters/m17.py &
+# sleep 5
+# python3 ./clusters/m18.py &
+# sleep 5
+# python3 ./clusters/m19.py &
+# sleep 5
+# python3 ./clusters/m20.py &
+# sleep 5
+# python3 ./clusters/m21.py &
+# sleep 5
+# python3 ./clusters/m22.py &
+# sleep 5
+# python3 ./clusters/m23.py &
+# sleep 5
+# python3 ./clusters/m24.py &
+# sleep 5
+# python3 ./clusters/m25.py &
+# sleep 5
+# python3 ./clusters/m26.py &
+# sleep 5
+# python3 ./clusters/m27.py &
+# sleep 5
+# python3 ./clusters/m28.py &
+# sleep 5
+# python3 ./clusters/m29.py &
+# sleep 5
+# python3 ./clusters/m30.py &
+
+chmod 777 02_check.sh
+chmod 777 03_system_ready.sh
+chmod 777 04.del.sh
---
+++ back/2-clusters-parasilo/04.del.sh
@@ -0,0 +1,31 @@
+python3 ./delcluster/management.py
+python3 ./delcluster/m1.py
+# python3 ./delcluster/m2.py
+# python3 ./delcluster/m3.py
+# python3 ./delcluster/m4.py
+# python3 ./delcluster/m5.py
+# python3 ./delcluster/m6.py
+# python3 ./delcluster/m7.py
+# python3 ./delcluster/m8.py
+# python3 ./delcluster/m9.py
+# python3 ./delcluster/m10.py
+# python3 ./delcluster/m11.py
+# python3 ./delcluster/m12.py
+# python3 ./delcluster/m13.py
+# python3 ./delcluster/m14.py
+# python3 ./delcluster/m15.py
+# python3 ./delcluster/m16.py
+# python3 ./delcluster/m17.py
+# python3 ./delcluster/m18.py
+# python3 ./delcluster/m19.py
+# python3 ./delcluster/m20.py
+# python3 ./delcluster/m21.py
+# python3 ./delcluster/m22.py
+# python3 ./delcluster/m23.py
+# python3 ./delcluster/m24.py
+# python3 ./delcluster/m25.py
+# python3 ./delcluster/m26.py
+# python3 ./delcluster/m27.py
+# python3 ./delcluster/m28.py
+# python3 ./delcluster/m29.py
+# python3 ./delcluster/m30.py
