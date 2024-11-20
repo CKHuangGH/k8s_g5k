24e94ed913ef69d43623ff1645419b34bf500213
CKHuangGH <chihkaihuang1@gmail.com>
2024-11-20 15:09:00 +0100

    rrr

--- back/2clusters/02_system_ready.sh
+++ back/2clusters/02_system_ready.sh
@@ -1,33 +1,54 @@
 #!/bin/bash
 i=0
 manage=$(awk NR==1 node_list)
-third_octet=$(echo "$ip_address" | awk -F. '{print $3}')
 git clone https://github.com/CKHuangGH/mck8s_vm.git
 git clone https://github.com/CKHuangGH/acala.git
+git clone https://github.com/CKHuangGH/k8s_google_injection.git
+git clone https://github.com/CKHuangGH/acala_error.git
 rm -rf /home/chuang/.ssh/known_hosts
 
 for j in $(cat node_list)
 do
-scp -r /home/chuang/k8s_g5k/mck8s_vm root@$j:/root/
-scp -r /home/chuang/k8s_g5k/acala root@$j:/root/
+#scp ./install.sh root@$j:/root/
+scp -r ./mck8s_vm root@$j:/root/
+scp -r ./acala root@$j:/root/
+scp -r ./k8s_google_injection root@$j:/root/
+scp -r ./acala_error root@$j:/root/
 scp /home/chuang/.ssh/id_rsa root@$j:/root/.ssh
-ssh -o StrictHostKeyChecking=no root@$j sudo apt-get update
-ssh -o StrictHostKeyChecking=no root@$j sudo apt-get install vim -y
-ssh -o StrictHostKeyChecking=no root@$j sudo apt install python3-pip -y
-ssh -o StrictHostKeyChecking=no root@$j sudo git clone https://github.com/CKHuangGH/acala_error.git
-ssh -o StrictHostKeyChecking=no root@$j sudo git clone https://github.com/CKHuangGH/k8s_google_injection.git
-ssh -o StrictHostKeyChecking=no root@$j sudo pip3 install pandas
-ssh -o StrictHostKeyChecking=no root@$j sudo pip3 install kubernetes
-ssh -o StrictHostKeyChecking=no root@$j sudo pip3 install aiohttp
-#ssh -o StrictHostKeyChecking=no root@$j mv /root/.kube/config /root/.kube/cluster$i
+#ssh -o StrictHostKeyChecking=no root@$j chmod 777 ./install.sh
+#ssh -o StrictHostKeyChecking=no root@$j sh ./install.sh &
+# ssh -o StrictHostKeyChecking=no root@$j sudo apt-get install vim -y
+#ssh -o StrictHostKeyChecking=no root@$j sudo apt install python3-pip -y
+# ssh -o StrictHostKeyChecking=no root@$j sudo pip3 install pandas
+#ssh -o StrictHostKeyChecking=no root@$j sudo pip3 install kubernetes
+# ssh -o StrictHostKeyChecking=no root@$j sudo pip3 install aiohttp
+# #ssh -o StrictHostKeyChecking=no root@$j mv /root/.kube/config /root/.kube/cluster$i
 ssh -o StrictHostKeyChecking=no root@$j scp -o StrictHostKeyChecking=no /root/.kube/config root@$manage:/root/.kube/cluster$i
-ssh -o StrictHostKeyChecking=no root@$j chmod 777 -r /root/mck8s_vm
-ssh -o StrictHostKeyChecking=no root@$j chmod 777 -r /root/acala/
-if 
+ssh -o StrictHostKeyChecking=no root@$j chmod 777 -R /root/mck8s_vm/
+ssh -o StrictHostKeyChecking=no root@$j chmod 777 -R /root/acala/
+ssh -o StrictHostKeyChecking=no root@$j chmod 777 -R /root/k8s_google_injection/
+ssh -o StrictHostKeyChecking=no root@$j chmod 777 -R /root/acala_error/
+# ssh -o StrictHostKeyChecking=no root@$j chmod 777 /root/acala/member/build.sh
+# ssh -o StrictHostKeyChecking=no root@$j chmod 777 /root/mck8s_vm/large-scale/00_acala.sh
+# ssh -o StrictHostKeyChecking=no root@$j chmod 777 /root/mck8s_vm/large-scale/00_prom.sh
+# ssh -o StrictHostKeyChecking=no root@$j chmod 777 /root/mck8s_vm/large-scale/01_joining.sh
+# ssh -o StrictHostKeyChecking=no root@$j chmod 777 /root/mck8s_vm/large-scale/acalavalue.sh
+# ssh -o StrictHostKeyChecking=no root@$j chmod 777 /root/mck8s_vm/large-scale/combineAll.sh
+# ssh -o StrictHostKeyChecking=no root@$j chmod 777 /root/mck8s_vm/large-scale/mck8s-management.sh
+# ssh -o StrictHostKeyChecking=no root@$j chmod 777 /root/mck8s_vm/large-scale/promvalue.sh
 i=$((i+1))
 done
 
-scp /home/chuang/.ssh/id_rsa root@10.158.$third_octet.3:/root/.ssh
-scp -r /home/chuang/k8s_g5k/mck8s_vm root@10.158.$third_octet.3:/root/
-scp node_list root@$manage:/root/mck8s_vm/node_list
+while read line
+do 
+echo $line
+ip1=$(echo $line | cut -d "." -f 2)
+ip2=$(echo $line | cut -d "." -f 3)
+break
+done < node_list
+
+
+scp /home/chuang/.ssh/id_rsa root@10.$ip1.$ip2.3:/root/.ssh
+scp -r ./mck8s_vm root@10.$ip1.$ip2.3:/root/
+scp node_list root@$manage:/root/mck8s_vm/large-scale/node_list
 echo "management node is $manage"
