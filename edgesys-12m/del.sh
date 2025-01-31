3ca394da0f02927e89a2f2e57b7191f31a821764
CKHuangGH <chihkaihuang1@gmail.com>
2025-01-31 14:17:27 +0100

    test

---
+++ edgesys-12m/del.sh
@@ -0,0 +1,8 @@
+folders=("/home/chuang/k8s_g5k/sdfcp-12m/mcdeploy-1" "/home/chuang/k8s_g5k/sdfcp-12m/mcdeploy-2" "/home/chuang/k8s_g5k/sdfcp-12m/mcdeploy-3")
+
+for folder in "${folders[@]}"; do
+    cd "$folder" || exit 1 
+    chmod +x 04_del.sh     
+    ./04_del.sh            
+    cd ..                  
+done
