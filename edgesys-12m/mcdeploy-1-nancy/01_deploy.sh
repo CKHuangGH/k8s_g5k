ae6a94cd099900bcb98da5c8a95ac0302ae1ab7c
CKHuang-public <95505429+CKHuangGH@users.noreply.github.com>
2025-01-31 17:59:29 +0100

    add nancy

---
+++ edgesys-12m/mcdeploy-1-nancy/01_deploy.sh
@@ -0,0 +1,32 @@
+python3 ./cluster/management.py
+sleep 60
+python3 ./cluster/m1.py &
+python3 ./cluster/m2.py
+# python3 ./cluster/m3.py &
+# python3 ./cluster/m4.py &
+# python3 ./cluster/m5.py &
+# python3 ./cluster/m6.py &
+# python3 ./cluster/m7.py &
+# python3 ./cluster/m8.py &
+# python3 ./cluster/m9.py &
+
+# python3 ./cluster/m10.py
+# python3 ./cluster/m41.py &
+# python3 ./cluster/m42.py &
+# python3 ./cluster/m43.py &
+# python3 ./cluster/m44.py &
+# python3 ./cluster/m45.py &
+# python3 ./cluster/m46.py &
+# python3 ./cluster/m47.py &
+# python3 ./cluster/m48.py &
+# python3 ./cluster/m49.py &
+# python3 ./cluster/m50.py
+
+echo "wait for 180 secs"
+sleep 600
+
+chmod 777 02_check.sh
+chmod 777 03_system_ready.sh
+chmod 777 04_del.sh
+
+. ./03_system_ready.sh
