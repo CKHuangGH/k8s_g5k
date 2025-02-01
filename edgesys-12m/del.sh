2ec4ebfc4cf75620a65e2fac340e810188631910
CKHuang-public <95505429+CKHuangGH@users.noreply.github.com>
2025-02-01 12:06:24 +0100

    Update del.sh

--- edgesys-12m/del.sh
+++ edgesys-12m/del.sh
@@ -1,4 +1,4 @@
-folders=("/home/chuang/k8s_g5k/edgesys-12m/mcdeploy-1-nancy" "/home/chuang/k8s_g5k/edgesys-12m/mcdeploy-1-nancy")
+folders=("/home/chuang/k8s_g5k/edgesys-12m/mcdeploy-1-nancy" "/home/chuang/k8s_g5k/edgesys-12m/mcdeploy-2-nancy" "/home/chuang/k8s_g5k/edgesys-12m/mcdeploy-3-nancy" "/home/chuang/k8s_g5k/edgesys-12m/mcdeploy-4-nancy")
 
 for folder in "${folders[@]}"; do
     cd "$folder" || exit 1 
