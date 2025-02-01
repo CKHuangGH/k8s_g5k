2f1acd1c0b874f32b5c88a31dbac65367f046326
CKHuang-public <95505429+CKHuangGH@users.noreply.github.com>
2025-02-02 00:47:52 +0100

    ww

--- edgesys-12m/mcdeploy-1/01_deploy.sh
+++ edgesys-12m/mcdeploy-1/01_deploy.sh
@@ -1,16 +1,20 @@
 python3 ./cluster/management.py
 sleep 60
-python3 ./cluster/m1.py
-#python3 ./cluster/m2.py
-# python3 ./cluster/m3.py &
-# python3 ./cluster/m4.py &
-# python3 ./cluster/m5.py &
-# python3 ./cluster/m6.py &
-# python3 ./cluster/m7.py &
-# python3 ./cluster/m8.py &
-# python3 ./cluster/m9.py &
-
-# python3 ./cluster/m10.py
+python3 ./cluster/m1.py &
+python3 ./cluster/m2.py &
+python3 ./cluster/m3.py &
+python3 ./cluster/m4.py &
+python3 ./cluster/m5.py &
+python3 ./cluster/m6.py &
+python3 ./cluster/m7.py &
+python3 ./cluster/m8.py &
+python3 ./cluster/m9.py &
+python3 ./cluster/m10.py &
+python3 ./cluster/m11.py &
+python3 ./cluster/m12.py &
+python3 ./cluster/m13.py &
+python3 ./cluster/m14.py &
+python3 ./cluster/m15.py
 # python3 ./cluster/m41.py &
 # python3 ./cluster/m42.py &
 # python3 ./cluster/m43.py &
@@ -26,9 +30,10 @@ chmod 777 02_check.sh
 chmod 777 03_system_ready.sh
 chmod 777 04_del.sh
 
-echo "wait for 60 secs"
-for (( i=60; i>0; i-- )); do
+echo "wait for 300 secs"
+for (( i=600; i>0; i-- )); do
     echo "$i secs..."
     sleep 1
 done
+
 . ./03_system_ready.sh
--- edgesys-12m/mcdeploy-2/01_deploy.sh
+++ edgesys-12m/mcdeploy-2/01_deploy.sh
@@ -9,24 +9,24 @@ python3 ./cluster/m6.py &
 python3 ./cluster/m7.py &
 python3 ./cluster/m8.py &
 python3 ./cluster/m9.py &
-python3 ./cluster/m10.py
-# python3 ./cluster/m41.py &
-# python3 ./cluster/m42.py &
-# python3 ./cluster/m43.py &
-# python3 ./cluster/m44.py &
-# python3 ./cluster/m45.py &
-# python3 ./cluster/m46.py &
-# python3 ./cluster/m47.py &
-# python3 ./cluster/m48.py &
-# python3 ./cluster/m49.py &
-# python3 ./cluster/m50.py
+python3 ./cluster/m10.py &
+python3 ./cluster/m11.py &
+python3 ./cluster/m12.py &
+python3 ./cluster/m13.py &
+python3 ./cluster/m14.py &
+python3 ./cluster/m15.py &
+python3 ./cluster/m16.py &
+python3 ./cluster/m17.py &
+python3 ./cluster/m18.py &
+python3 ./cluster/m19.py &
+python3 ./cluster/m20.py
 
 chmod 777 02_check.sh
 chmod 777 03_system_ready.sh
 chmod 777 04_del.sh
 
-echo "wait for 300 secs"
-for (( i=600; i>0; i-- )); do
+echo "wait for 700 secs"
+for (( i=700; i>0; i-- )); do
     echo "$i secs..."
     sleep 1
 done
